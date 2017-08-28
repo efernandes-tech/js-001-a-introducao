9b10ac8f576e0d41971bfe725b3cee5ff4931680
“edersonlrf” <edersonluis.rf@gmail.com>
2017-08-28 13:31:38 -0300

    A2 validando os dados do usuario

--- js/principal.js
+++ js/principal.js
@@ -7,11 +7,27 @@ var paciente = document.querySelector("#primeiro-paciente");
 var tdPeso = paciente.querySelector(".info-peso");
 var tdAltura = paciente.querySelector(".info-altura");
 
+var tdImc = paciente.querySelector(".info-imc");
+
 var peso = tdPeso.textContent;
 var altura = tdAltura.textContent;
 
-var imc = peso / (altura * altura);
+var alturaEhValida = true;
+var pesoEhValido = true;
 
-var tdImc = paciente.querySelector(".info-imc");
+if(peso <= 0 || peso > 1000){
+    console.log("Peso inválido");
+    tdImc.textContent = "Peso inválido!";
+    pesoEhValido = false;
+}
 
-tdImc.textContent = imc;
+if(altura <= 0 || altura >= 3){
+    console.log("Altura inválida");
+    tdImc.textContent = "Altura inválida!";
+    alturaEhValida = false;
+}
+
+if(pesoEhValido && alturaEhValida){
+    var imc = peso / ( altura * altura);
+    tdImc.textContent = imc;
+}
