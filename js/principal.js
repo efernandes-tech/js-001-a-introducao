14171b4bb16bb7220efa473b03fb6aba3a9a1730
edersonlrf <edersonluis.rf@gmail.com>
2017-09-02 20:00:35 -0300

    A4 criando o formulario de adicao de pacientes

--- js/principal.js
+++ js/principal.js
@@ -43,3 +43,10 @@ for(var i = 0; i < pacientes.length ; i++){
         tdImc.textContent = imc.toFixed(2);
     }
 }
+
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+
+botaoAdicionar.addEventListener("click", function(event) {
+    event.preventDefault();
+    console.log("Fui clicado!");
+});
