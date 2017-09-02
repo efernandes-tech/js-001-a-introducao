c0e6a40239cb7d16ae3e1362ce73ab87c1a6750b
edersonlrf <edersonluis.rf@gmail.com>
2017-09-02 20:08:12 -0300

    A4 adicionando um paciente na tabela

--- js/principal.js
+++ js/principal.js
@@ -48,5 +48,36 @@ var botaoAdicionar = document.querySelector("#adicionar-paciente");
 
 botaoAdicionar.addEventListener("click", function(event) {
     event.preventDefault();
-    console.log("Fui clicado!");
+
+    var form = document.querySelector("#form-adiciona");
+
+    // Um propriedade do form é o acesso direto aos inputs com name.
+    var nome = form.nome.value;
+    var peso = form.peso.value;
+    var altura = form.altura.value;
+    var gordura = form.gordura.value;
+
+    /* Criando os elementos. */
+    var pacienteTr = document.createElement("tr");
+
+    var nomeTd = document.createElement("td");
+    var pesoTd = document.createElement("td");
+    var alturaTd = document.createElement("td");
+    var gorduraTd = document.createElement("td");
+    var imcTd = document.createElement("td");
+
+    nomeTd.textContent = nome;
+    pesoTd.textContent = peso;
+    alturaTd.textContent = altura;
+    gorduraTd.textContent = gordura;
+
+    /* Adicionando os elementos na tabela. */
+    pacienteTr.appendChild(nomeTd);
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd);
+
+    var tabela = document.querySelector("#tabela-pacientes");
+
+    tabela.appendChild(pacienteTr);
 });
