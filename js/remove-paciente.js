ef1773c5c7d96808720771cc5a5c68cb7be4975f
edersonlrf <--global>
2017-09-20 17:37:55 -0300

    A7 animando a remocao

--- js/remove-paciente.js
+++ js/remove-paciente.js
@@ -3,6 +3,10 @@ var tabela = document.querySelector("#tabela-pacientes");
 tabela.addEventListener("dblclick", function(event) {
     // Somente executará nosso código caso o elemento em que clicamos seja um <td>.
     if (event.target.tagName == 'TD') {
-        event.target.parentNode.remove()
+        event.target.parentNode.classList.add("fadeOut");
+
+        setTimeout(function() {
+            event.target.parentNode.remove();
+        }, 500);
     }
 });
