99c2c4568525632741a5c902dbe3873eb8c3a185
edersonlrf <--global>
2017-09-20 19:27:35 -0300

    A8 buscando pacientes com expressao regular

--- js/filtra.js
+++ js/filtra.js
@@ -6,7 +6,17 @@ campoFiltro.addEventListener("input", function() {
     if (this.value.length > 0) {
         for (var i = 0; i < pacientes.length; i++) {
             var paciente = pacientes[i];
-            paciente.classList.add("invisivel");
+            var tdNome = paciente.querySelector(".info-nome");
+            var nome = tdNome.textContent;
+
+            // Filtrando com expressão regular.
+            var expressao = new RegExp(this.value, "i"); // i -> case-insensitive -> Pa == pa
+
+            if (expressao.test(nome)) {
+                paciente.classList.remove("invisivel");
+            } else {
+                paciente.classList.add("invisivel");
+            }
         }
     } else {
         for (var i = 0; i < pacientes.length; i++) {
